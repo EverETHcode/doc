aaa4e7ccca285fa74177f0cabbf1e0b2e2ef3ebb
EverETH <89136637+EverETHcode@users.noreply.github.com>
2023-10-09 02:03:03 -0700

    update

--- sidebars.js
+++ sidebars.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       type: 'html',
       value:
-        '<a class="navbar__brand" href="/"><div class="navbar__logo"><img src="/img/walletconnect-logo.png#dark-mode-only"  alt="WalletConnect Logo"><img src="/img/walletconnect-logo-black.svg#light-mode-only"  alt="WalletConnect Logo"></div>WalletConnect<span>Docs<span></a>',
+        '<a class="navbar__brand" href="/"><div class="navbar__logo"><img src="/img/walletconnect-logo-white.png#dark-mode-only"  alt="WalletConnect Logo"><img src="/img/walletconnect-logo-black.svg#light-mode-only"  alt="WalletConnect Logo"></div>EverETH<span>Docs<span></a>',
       defaultStyle: true
     },
     {
